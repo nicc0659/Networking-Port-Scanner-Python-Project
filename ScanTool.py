ee993f1454e807cb9a0b88a757914b13aaaed251
nicc0660 <nicc0660@NC-SurfaceBook2>
2023-05-22 22:26:42 +0200

    pt2

--- ScanTool.py
+++ ScanTool.py
@@ -1,169 +1,58 @@
-import sys
-import pyshark
-import nmap
-from PyQt5.QtCore import Qt, pyqtSignal, QThread, QObject, pyqtSlot
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QPushButton, QVBoxLayout, QWidget, QProgressBar, QSizePolicy
-from PyQt5.QtGui import QFont
+import socket
+import threading
+import tkinter as tk
+from tkinter import ttk
 
-class NetworkScanner(QObject):
-    """A class for performing network scans using Pyshark."""
-    progress_callback = pyqtSignal(int)
+def portScanner(host):
+    open_ports = []
+    for port in range(1, 64535):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(5)
+        result = sock.connect_ex((host, port))
+        if result == 0:
+            logText.insert(tk.END, f'Port {port} is open\n')
+            open_ports.append(port)
+        else:
+            logText.insert(tk.END, f'Port {port} is closed\n')
+        logText.see(tk.END)
+        sock.close()
+        progressBar['value'] = port / 64535 * 100
+        root.update_idletasks()
+    return open_ports
 
-    def __init__(self, interface='eth0'):
-        """Initialize the NetworkScanner instance with the specified interface."""
-        super().__init__()
-        self.interface = interface
+def startScan():
+    host = hostEntry.get()
+    open_ports = portScanner(host)
+    resultLabel['text'] = f'Open ports on {host}:\n' + '\n'.join(map(str, open_ports))
 
-    def start_network_scan(self, packet_count=10):
-        """Start a network scan using Pyshark and return the captured packets."""
-        try:
-            packets = []
-            capture = pyshark.LiveCapture(interface=self.interface)
-            for i, packet in enumerate(capture.sniff_continuously(packet_count=packet_count)):
-                packets.append(packet)
-                self.progress_callback.emit(int((i + 1) / packet_count * 100))
-                if i + 1 >= packet_count:
-                    break
-            capture.close()
-            return packets
-        except pyshark.capture.capture.TSharkCrashException as e:
-            print(f"Error capturing packets: {e}")
-            return []
+root = tk.Tk()
+root.title('Port Scanner')
 
-class PortScanner(QObject):
-    """A class for performing port scans using nmap."""
-    progress_callback = pyqtSignal(int)
+hostLabel = tk.Label(root, text='Host:')
+hostLabel.pack()
 
-    def __init__(self):
-        """Initialize the PortScanner instance with the default target."""
-        super().__init__()
-        self.target = 'localhost'
+hostEntry = tk.Entry(root)
+hostEntry.pack()
 
-    def start_port_scan(self):
-        """Start a port scan using nmap and return a list of ports and their state."""
-        open_ports = []
-        port_scanner = nmap.PortScanner()
-        result = port_scanner.scan(self.target, arguments='-p 1-65535')
-        total_ports = sum(len(result['scan'][host]['tcp']) for host in result['scan'] if 'tcp' in result['scan'][host])
-        scanned_ports = 0
-        for host in result['scan']:
-            if 'tcp' in result['scan'][host]:
-                ports = result['scan'][host]['tcp']
-                for port in ports:
-                    if result['scan'][host]['tcp'][port]['state'] == 'open':
-                        open_ports.append(f"Port {port} is open")
-                    scanned_ports += 1
-                    self.progress_callback.emit(int((scanned_ports / total_ports) * 100))
-        return open_ports
+scanButton = tk.Button(root, text='Start Scan', command=startScan)
+scanButton.pack()
 
-class MainWindow(QMainWindow):
-    """The main window of the application."""
-    def __init__(self):
-        """Initialize the MainWindow instance with the GUI elements and scanners."""
-        super().__init__()
+progressBar = ttk.Progressbar(root, orient='horizontal', length=200, mode='determinate')
+progressBar.pack()
 
-        self.setWindowTitle("Network and Port Scanner")
-        self.setMinimumSize(600, 400)
+logFrame = tk.Frame(root)
+logFrame.pack()
 
-        self.text_area = QTextEdit(self)
-        self.text_area.setReadOnly(True)
-        self.text_area.setFont(QFont("Monospace", 10))
-        self.scan_network_button = QPushButton("Scan Network", self)
-        self.scan_ports_button = QPushButton("Scan Ports", self)
-        self.progress_bar = QProgressBar(self)
-        self.progress_bar.setAlignment(Qt.AlignCenter)
-        self.progress_bar.setMinimum(0)
-        self.progress_bar.setMaximum(100)
-        self.progress_bar.setVisible(False)
+logText = tk.Text(logFrame, height=10)
+logText.pack(side=tk.LEFT)
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.text_area)
-        layout.addWidget(self.scan_network_button)
-        layout.addWidget(self.scan_ports_button)
-        layout.addWidget(self.progress_bar)
+logScrollbar = tk.Scrollbar(logFrame)
+logScrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
-        widget = QWidget()
-        widget.setLayout(layout)
-        self.setCentralWidget(widget)
+logText.config(yscrollcommand=logScrollbar.set)
+logScrollbar.config(command=logText.yview)
 
-        self.network_scanner = NetworkScanner()
-        self.port_scanner = PortScanner()
+resultLabel = tk.Label(root, text='')
+resultLabel.pack()
 
-        self.scan_network_button.clicked.connect(self.start_network_scan)
-        self.scan_ports_button.clicked.connect(self.start_port_scan)
-
-    @pyqtSlot()
-    def start_network_scan(self):
-        """Start a network scan and display the captured packets in the text area."""
-        self.text_area.clear()
-        self.text_area.append("Starting network scan...")
-        self.progress_bar.setValue(0)
-        self.progress_bar.setVisible(True)
-        self.network_thread = NetworkThread(self.network_scanner)
-        self.network_thread.progress_callback.connect(self.update_progress_bar)
-        self.network_thread.result_callback.connect(self.display_packets)
-        self.network_thread.finished.connect(self.network_thread.deleteLater)
-        self.network_thread.start()
-
-    @pyqtSlot()
-    def start_port_scan(self):
-        """Start a port scan and display the open ports in the text area."""
-        self.text_area.clear()
-        self.text_area.append("Starting port scan...")
-        self.progress_bar.setValue(0)
-        self.progress_bar.setVisible(True)
-        self.port_thread = PortThread(self.port_scanner)
-        self.port_thread.progress_callback.connect(self.update_progress_bar)
-        self.port_thread.result_callback.connect(self.display_ports)
-        self.port_thread.finished.connect(self.port_thread.deleteLater)
-        self.port_thread.start()
-
-    @pyqtSlot(int)
-    def update_progress_bar(self, value):
-        """Update the progress bar value."""
-        self.progress_bar.setValue(value)
-
-    @pyqtSlot(list)
-    def display_packets(self, packets):
-        """Display the captured packets in the text area."""
-        for packet in packets:
-            self.text_area.append(str(packet))
-
-    @pyqtSlot(list)
-    def display_ports(self, ports):
-        """Display the open ports in the text area."""
-        for port in ports:
-            self.text_area.append(port)
-
-
-class NetworkThread(QThread):
-    """A thread for running the network scan."""
-    progress_callback = pyqtSignal(int)
-    result_callback = pyqtSignal(list)
-
-    def __init__(self, network_scanner):
-        super().__init__()
-        self.network_scanner = network_scanner
-
-    def run(self):
-        packets = self.network_scanner.start_network_scan()
-        self.result_callback.emit(packets)
-
-class PortThread(QThread):
-    """A thread for running the port scan."""
-    progress_callback = pyqtSignal(int)
-    result_callback = pyqtSignal(list)
-
-    def __init__(self, port_scanner):
-        super().__init__()
-        self.port_scanner = port_scanner
-
-    def run(self):
-        ports = self.port_scanner.start_port_scan()
-        self.result_callback.emit(ports)
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec_())
+root.mainloop()
--- ScanTool2.py
+++ ScanTool2.py
@@ -1,92 +1,63 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QTextEdit, QPushButton, QProgressBar, QLabel
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QFontDatabase, QIcon
-from PyQt5.QtSvg import QSvgWidget
+from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivy.clock import Clock
+from kivy.properties import StringProperty
+from kivymd.app import MDApp
+import threading
+import socket
 
+KV = """
+BoxLayout:
+    orientation: 'vertical'
+    MDToolbar:
+        title: 'Port Scanner'
+    BoxLayout:
+        orientation: 'horizontal'
+        MDTextField:
+            id: host
+            hint_text: 'Host'
+        MDRaisedButton:
+            text: 'Start Scan'
+            on_release: app.startScan()
+    ScrollView:
+        MDList:
+            id: log_list
+    MDSpinner:
+        id: spinner
+        size_hint: None, None
+        size: dp(46), dp(46)
+        pos_hint: {'center_x': .5, 'center_y': .5}
+        active: False
+"""
 
-class NetworkScanner:
-    """A class for performing network scans."""
+def portScanner(host, log_list, spinner):
+    open_ports = []
+    for port in range(1, 64535):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(5)
+        result = sock.connect_ex((host, port))
+        if result == 0:
+            log_list.add_widget(
+                OneLineListItem(text=f'Port {port} is open')
+            )
+            open_ports.append(port)
+        else:
+            log_list.add_widget(
+                OneLineListItem(text=f'Port {port} is closed')
+            )
+        sock.close()
+    spinner.active = False
 
-    def __init__(self, interface='eth0', target='localhost'):
-        """Initialize the NetworkScanner instance with the specified interface and target."""
-        self.interface = interface
-        self.target = target
+class PortScannerApp(MDApp):
+    def build(self):
+        return Builder.load_string(KV)
 
-    def start_network_scan(self, packet_count=10):
-        """Start a network scan and return the captured packets."""
-        # Your network scan logic here
-        pass
+    def startScan(self):
+        host = self.root.ids.host.text
+        log_list = self.root.ids.log_list
+        log_list.clear_widgets()
+        spinner = self.root.ids.spinner
+        spinner.active = True
+        threading.Thread(target=portScanner, args=(host, log_list, spinner)).start()
 
-
-class PortScanner:
-    """A class for performing port scans."""
-
-    def __init__(self, target='localhost'):
-        """Initialize the PortScanner instance with the specified target."""
-        self.target = target
-
-    def start_port_scan(self):
-        """Start a port scan and return a list of open ports."""
-        # Your port scan logic here
-        pass
-
-
-class MainWindow(QMainWindow):
-    """The main window of the application."""
-
-    def __init__(self):
-        super().__init__()
-
-        self.setWindowTitle("Network and Port Scanner")
-        self.setWindowIcon(QIcon("icon.svg"))
-
-        layout = QVBoxLayout()
-
-        self.text_area = QTextEdit()
-        self.text_area.setReadOnly(True)
-        layout.addWidget(self.text_area)
-
-        scan_network_button = QPushButton("Scan Network")
-        scan_network_button.clicked.connect(self.start_network_scan)
-        layout.addWidget(scan_network_button)
-
-        scan_ports_button = QPushButton("Scan Ports")
-        scan_ports_button.clicked.connect(self.start_port_scan)
-        layout.addWidget(scan_ports_button)
-
-        self.progress_bar = QProgressBar()
-        self.progress_bar.setRange(0, 100)
-        layout.addWidget(self.progress_bar)
-
-        central_widget = QWidget()
-        central_widget.setLayout(layout)
-        self.setCentralWidget(central_widget)
-
-    def start_network_scan(self):
-        """Start a network scan and display the captured packets in the text area."""
-        self.text_area.setPlainText("Starting network scan...")
-        self.progress_bar.setValue(0)
-
-        network_scanner = NetworkScanner()
-        network_scanner.start_network_scan()
-
-    def start_port_scan(self):
-        """Start a port scan and display the open ports in the text area."""
-        self.text_area.setPlainText("Starting port scan...")
-        self.progress_bar.setValue(0)
-
-        port_scanner = PortScanner()
-        port_scanner.start_port_scan()
-
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-
-    # Load the Material Design icons
-    QFontDatabase.addApplicationFont("materialdesignicons.ttf")
-
-    window = MainWindow()
-    window.show()
-
-    sys.exit(app.exec_())
+PortScannerApp().run()
